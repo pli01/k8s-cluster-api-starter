868f297263e3fd869f3be2fa46c2a9a5dbc53b6d
pli01 <pli01@users.noreply.github.com>
2024-05-14 23:15:34 +0200

    kubeconfig

--- openstack/deploy/install_ingress-nginx.sh
+++ openstack/deploy/install_ingress-nginx.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 #
 CLUSTER_NAME="${1:? argument not defined. run ./configure_cluster my-cluster-name }"
+
+kubeconfig_dir="$(dirname $0)"
+KUBECONFIG="$kubeconfig_dir/${CLUSTER_NAME}.kubeconfig"
+
+if [ ! -f "${KUBECONFIG}" ] ; then
+  echo "${KUBECONFIG} not found"
+  exit 1
+fi
+
+kubectl_args="--kubeconfig=${KUBECONFIG}"
+
 #
 # install helm ingress-nginx with overrided values
 # For demo, external LB is provsionned in openstack outside cluster api
 #   - ingress-nginx is configured in NodePort mode (not LoadBalancer)
 #   - trafic from LB is routed to ingress worker node hosting ingress-nginx
 #
-helm --kubeconfig=${CLUSTER_NAME}.kubeconfig upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace -f sample-demo/ingress-nginx-values.yaml  --wait-for-jobs  --wait
+helm ${kubectl_args} upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace -f sample-demo/ingress-nginx-values.yaml  --wait-for-jobs  --wait
 
--- openstack/deploy/install_whoami.sh
+++ openstack/deploy/install_whoami.sh
@@ -4,4 +4,14 @@
 #
 CLUSTER_NAME="${1:? argument not defined. run ./configure_cluster my-cluster-name }"
 
-kubectl --kubeconfig=${CLUSTER_NAME}.kubeconfig  apply -f sample-demo/whoami.yaml
+kubeconfig_dir="$(dirname $0)"
+KUBECONFIG="$kubeconfig_dir/${CLUSTER_NAME}.kubeconfig"
+
+if [ ! -f "${KUBECONFIG}" ] ; then
+  echo "${KUBECONFIG} not found"
+  exit 1
+fi
+
+kubectl_args="--kubeconfig=${KUBECONFIG}"
+
+kubectl ${kubectl_args} apply -f sample-demo/whoami.yaml
