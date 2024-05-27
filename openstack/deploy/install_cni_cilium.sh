afcd08e95afb34326ab5f73f19e663523bfe3759
pli01 <pli01@users.noreply.github.com>
2024-05-27 17:27:26 +0200

    doc and quick script

---
+++ openstack/deploy/install_cni_cilium.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+CLUSTER_NAME="${1:? argument not defined. run ./configure_cluster my-cluster-name }"
+
+kubeconfig_dir="$(dirname $0)"
+KUBECONFIG="$kubeconfig_dir/${CLUSTER_NAME}.kubeconfig"
+
+if [ ! -f "${KUBECONFIG}" ] ; then
+  echo "${KUBECONFIG} not found"
+  exit 1
+fi
+
+kubectl_args="--kubeconfig=${KUBECONFIG}"
+
+CILIUM_VERSION=1.15.5
+
+helm ${kubectl_args} upgrade --install cilium cilium \
+  --repo https://helm.cilium.io/ \
+  --version ${CILIUM_VERSION}  \
+  -f sample-demo/cni-cilium-values.yaml \
+  --namespace kube-system \
+  --create-namespace --wait-for-jobs --wait
