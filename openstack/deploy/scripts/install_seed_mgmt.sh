43ce218c5150cbc7f7d67595aaae758df360c319
pli <pli01@users.noreply.github.com>
2024-09-24 11:44:38 +0200

    Bootstrap (#25)
    * add bootstrap
    
    * install seed
    
    * fix sample
    
    * fix bootstrap

--- openstack/deploy/scripts/install_seed_mgmt.sh
+++ openstack/deploy/scripts/install_seed_mgmt.sh
@@ -2,25 +2,25 @@
 #
 # install some tools (kind, kubectl...)
 # source VERSION
-KIND_VERSION="${KIND_VERSION:-v0.24.0}"
-KUBECTL_VERSION="${KUBECTL_VERSION:-v1.29.9}"
-KUBECTL_VERSION="$(curl -Lfs $KUBECTL_RELEASE_VERSION/stable.txt)"
-HELM_VERSION="${HELM_VERSION:-v3.16.1}"
-HELMFILE_VERSION="${HELMFILE_VERSION:-0.168.0}"
-MKCERT_VERSION="${MKCERT_VERSION:-v1.4.4}"
-AGE_VERSION="${AGE_VERSION:-v1.2.0}"
-SOPS_VERSION="${SOPS_VERSION:-v3.8.1}"
+export KIND_VERSION="${KIND_VERSION:-v0.24.0}"
+export KUBECTL_VERSION="${KUBECTL_VERSION:-v1.29.9}"
+export KUBECTL_VERSION="$(curl -Lfs $KUBECTL_RELEASE_VERSION/stable.txt)"
+export HELM_VERSION="${HELM_VERSION:-v3.16.1}"
+export HELMFILE_VERSION="${HELMFILE_VERSION:-0.168.0}"
+export MKCERT_VERSION="${MKCERT_VERSION:-v1.4.4}"
+export AGE_VERSION="${AGE_VERSION:-v1.2.0}"
+export SOPS_VERSION="${SOPS_VERSION:-v3.8.1}"
 
 curl -L https://raw.githubusercontent.com/numerique-gouv/dk8s/main/scripts/install-prereq.sh | bash
 
 # install clusterctl
-CLUSTERCTL_VERSION=v1.8.3
+export CLUSTERCTL_VERSION=${CLUSTERCTL_VERSION:-v1.8.3}
 curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/${CLUSTERCTL_VERSION}/clusterctl-linux-amd64 -o clusterctl
 chmod +x clusterctl
 sudo mv clusterctl /usr/local/bin/clusterctl
 
 # install yq
-YQ_VERSION=v4.44.3
+export YQ_VERSION=${YQ_VERSION:-v4.44.3}
 curl -LO https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64.tar.gz
 tar -zxvf yq_linux_amd64.tar.gz  ./yq_linux_amd64
 rm -rf yq_linux_amd64.tar.gz
